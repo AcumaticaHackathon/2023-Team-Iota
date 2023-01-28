65dc7f7e4b9d846d775768369a010866853d1a40
Scott Stanaland <scott.stanaland@avantehs.com>
2023-01-28 17:05:24 -0500

    initial implement interface

---
+++ AcuConvert.Sage500/Sage500.cs
@@ -0,0 +1,18 @@
+﻿using AcuConvert.Core;
+using AcuConvert.Core.Models;
+
+using System.Data.SqlClient;
+namespace AcuConvert.Sage500;
+
+public class Sage500 : AcuConvert.Core.Interfaces.ILegacyConnector
+{
+    public IEnumerable<Row> GetDataSet(DateTime lastRunDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Field> GetSchema(EntityType type)
+    {
+        throw new NotImplementedException();
+    }
+}
