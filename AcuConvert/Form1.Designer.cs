f694600ef8fe7d22584aba7836cebfbaf607c53d
kyle <kyle@contou.com>
2023-01-28 22:39:34 -0800

    asdf

--- AcuConvert/Form1.Designer.cs
+++ AcuConvert/Form1.Designer.cs
@@ -564,6 +564,7 @@ partial class Form1
             this.button1.TabIndex = 2;
             this.button1.Text = "Prepare";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- AcuConvert/Form1.cs
+++ AcuConvert/Form1.cs
@@ -2,7 +2,9 @@ using System.ComponentModel;
 using AcuConvert.Core.Interfaces;
 using System.Data.SqlClient;
 using AcuConvert.Core.Models;
+using AcuConvert.Core.Models.Data;
 using AcuConvert.Core.Models.Data.DB;
+using AcuConvert.Core.Workers;
 
 namespace AcuConvert;
 
@@ -10,20 +12,25 @@ public partial class Form1 : Form
 {
     public           BindingList<SyncMapping> SyncMappings { get; set; }
     public           BindingList<SyncRow>     SyncRows     { get; set; }
+    public           BindingList<SyncField>   SyncFields   { get; set; }
+    public           SyncInstance             Instance     { get; set; }
     private readonly ISyncRepository          _syncRepository;
     private readonly ISyncWorker              _syncWorker;
     private readonly IAcumaticaConnector      _acumaticaConnector;
     private readonly ILegacyConnector         _legacyConnector;
+    private readonly RecordReconciliator      _reconciliator;
 
     public Form1(ISyncRepository syncRepository,
                  ISyncWorker syncWorker,
                  IAcumaticaConnector acumaticaConnector,
-                 ILegacyConnector legacyConnector)
+                 ILegacyConnector legacyConnector,
+                 RecordReconciliator reconciliator)
     {
-        _syncRepository       = syncRepository;
-        _syncWorker           = syncWorker;
-        _acumaticaConnector   = acumaticaConnector;
-        _legacyConnector = legacyConnector;
+        _syncRepository     = syncRepository;
+        _syncWorker         = syncWorker;
+        _acumaticaConnector = acumaticaConnector;
+        _legacyConnector    = legacyConnector;
+        _reconciliator = reconciliator;
         InitializeComponent();
         
     }
@@ -35,6 +42,8 @@ public partial class Form1 : Form
         
         SyncRows                 = new BindingList<SyncRow>(_syncRepository.GetSyncDataSet("Customer").ToList());
         grdExceptions.DataSource = SyncRows;
+
+        this.Instance = _syncRepository.GetSyncInstance("customer");
 
     }
 
@@ -64,5 +73,54 @@ public partial class Form1 : Form
                 });
             }
         }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        // Prepare data
+        var items = _legacyConnector.GetDataSet(new LegacyConnectionContext()
+        {
+            AuthenticationValues = _syncRepository.GetSourceConnectionSettings().ToArray(),
+            QueryParameters = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("Query", this.txtFromCustomer.Text + " " + this.txtWhereCustomer.Text)
+            }
+        }, Instance.LastRun ?? new DateTime(1900, 1, 1));
+
+        // Dictionary<int, Row> rowDic = new Dictionary<int, Row>();
+        // foreach (SyncRow row in SyncRows)
+        // {
+        //     rowDic.Add(row.RowNbr, new Row("Customer",row.RowNbr)
+        //     {
+        //         NoteId = row.NoteID
+        //     });
+        // }
+        // _reconciliator.SyncRow();
+        SyncRows.Clear();
+        int rowNbr = 1;
+        foreach (Row item in items)
+        {
+            SyncRows.Add(new SyncRow()
+            {
+                InstanceID = "Customer",
+                RowNbr = rowNbr,
+                Selected = false
+            });
+
+            foreach (KeyValuePair<string, Field> field in item.Fields)
+            {
+                SyncFields.Add(new SyncField()
+                {
+                    DataType = "String",
+                    InstanceID = "Customer",
+                    RowNbr = rowNbr,
+                    SourceFieldName = field.Value.FieldName,
+                    Value = field.Value.Value
+                });
+            }
+
+            rowNbr++;
+        }
+        Instance.LastRun = DateTime.Now;
     }
 }
