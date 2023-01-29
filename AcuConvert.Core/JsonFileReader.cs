6dbfd728ca05d36e36742acf9f1c2358c955dcd6
Gihan <gihan.desilva@acumatica.com>
2023-01-29 07:27:06 +0530

    Add Sample swagger reader from a file

---
+++ AcuConvert.Core/JsonFileReader.cs
@@ -0,0 +1,24 @@
+﻿using AcuConvert.Core.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcuConvert.Core
+{
+    internal class JsonFileReader : IJasonInput
+    {
+        public string GetSwaggerJsonString()
+        {
+            string json = "";
+            using (StreamReader r = new StreamReader(@"..\..\..\..\AcuConvert.Core\SampleJson\sample-swagger.json"))
+            {
+                json = r.ReadToEnd();
+            }
+
+            return json;
+        }
+    }
+}
